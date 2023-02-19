49e4c2a451bc344ea159aaee405a153281c81841
zORg <zorgalex22@gmail.com>
2023-02-19 13:24:38 +0200

    Sample 1

--- Samples/Sample1Script.cs
+++ Samples/Sample1Script.cs
@@ -13,6 +13,7 @@ namespace BezierCurveZ.Samples {
 	{
 		public Curve profile;
 		public Curve path;
+		public List<Curve> curves;
 		public Vector3 scale = Vector3.one;
 		public Vector3 offset;
 		public ProfileUtility.UVMode mode;
@@ -20,14 +21,33 @@ namespace BezierCurveZ.Samples {
 		public void Generate()
 		{
 			var mf = GetComponent<MeshFilter>();
-			var mesh = ProfileUtility.GenerateProfileMesh(path, profile, offset, scale, mode: mode);
-			mf.sharedMesh = mesh;
+			var meshes = new List<Mesh>
+			{
+				ProfileUtility.GenerateProfileMesh(path, profile, offset, scale, mode: mode)
+			};
+			foreach (var curve in curves)
+			{
+				meshes.Add(ProfileUtility.GenerateProfileMesh(curve, profile, offset, scale, mode: mode));
+			}
+			CombineInstance[] combine = new CombineInstance[meshes.Count];
+			for (int i = 0; i < meshes.Count; i++)
+			{
+				combine[i] = new CombineInstance() { mesh= meshes[i], transform = Matrix4x4.identity };
+			}
+			var combinedMesh = new Mesh();
+			combinedMesh.CombineMeshes(combine, true);
+
+			mf.sharedMesh = combinedMesh;
 		}
 	}
 
 #if UNITY_EDITOR
 	[CustomEditor(typeof(Sample1Script))]
+#if ODIN_INSPECTOR
+	public class Sample1ScriptInspector : Sirenix.OdinInspector.Editor.OdinEditor
+#else
 	public class Sample1ScriptInspector : Editor
+#endif
 	{
 		public override void OnInspectorGUI()
 		{
