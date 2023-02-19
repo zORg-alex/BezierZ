d9470414e98369cf0fc88f7950978230c066c4b0
zORg <zorgalex22@gmail.com>
2023-02-19 12:56:43 +0200

    Extrude fix; Debug stepthrough for some properties;
    Removed not working feature of reverse extrude

--- Scripts/Curve.cs
+++ Scripts/Curve.cs
@@ -229,7 +229,9 @@ namespace BezierCurveZ
 		/// Updates point position. Used in case point parameters changed that can influence other points.
 		/// </summary>
 		public void UpdatePosition(int index) => SetPointPosition(index, _points[index]);
+		[DebuggerStepThrough]
 		public void SetPointPosition(int index, Vector3 position) => SetPointPosition((ushort)index, position, true);
+		[DebuggerStepThrough]
 		public void SetPointPosition(int index, Vector3 position, bool recursive = true) => SetPointPosition((ushort)index, position, recursive);
 		public void SetPointPosition(ushort index, Vector3 position, bool recursive = true)
 		{
